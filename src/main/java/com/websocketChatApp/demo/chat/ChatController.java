8f12eb92c43618f476d7c4155a617e6e88eb2d47
Wealthometer <wealthhajoh87@gmail.com>
2025-09-18 23:20:08 +0100

    Update ChatController

--- src/main/java/com/websocketChatApp/demo/chat/ChatController.java
+++ src/main/java/com/websocketChatApp/demo/chat/ChatController.java
@@ -3,6 +3,7 @@ package com.websocketChatApp.demo.chat;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,7 +14,17 @@ public class ChatController {
     public ChatMessage sendMessage (
             @Payload ChatMessage chatMessage
     ) {
-        return null;
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(
+            @Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor
+    ) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
     }
 
 }
