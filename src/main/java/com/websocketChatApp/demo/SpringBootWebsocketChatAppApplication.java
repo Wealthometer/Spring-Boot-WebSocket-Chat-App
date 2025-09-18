044f5b61764e505b806d03aa2094a7dbc73284d9
Wealthometer <wealthhajoh87@gmail.com>
2025-09-18 22:27:45 +0100

    java java 😂😹🤣

---
+++ src/main/java/com/websocketChatApp/demo/SpringBootWebsocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.websocketChatApp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebsocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocketChatApp/demo/SpringBootWebsocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocketChatApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
